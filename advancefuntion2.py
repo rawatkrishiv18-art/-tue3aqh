1587f0281bdae2337f7a03f019abf70f6af0ebb3
rawatkrishiv18-art <rawatkrishiv18@gmail.com>
2026-02-16 19:40:49 +0530

    Add files via upload

---
+++ advancefuntion2.py
@@ -0,0 +1,12 @@
+s1 = {2,3,1}
+s2 = {'b','a','c'}
+s3 = list(zip(s1, s2))
+print(s3)
+list1 = [10,20,30,40]
+list2 = [100,200,300,400]
+for x,y in zip(list1, list2[::-1]):
+    print(x,y)
+stocks = ['reliance','infosys','tcs ']
+prices = [2000, 1500, 3000]
+new_dict = {stocks:prices for stocks, prices in zip(stocks, prices)}
+print(format(new_dict))
