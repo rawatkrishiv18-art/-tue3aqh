94227f3064730a3f4c0c8c7d5d8422da604b7e80
rawatkrishiv18-art <rawatkrishiv18@gmail.com>
2026-02-23 19:25:09 +0530

    Add files via upload

---
+++ obj2.py
@@ -0,0 +1,9 @@
+class vehicle:
+    def __init__(self, max_speed, milage, ):
+        self.max_speed = max_speed
+        self.milage = milage
+modelX = vehicle(240, 18)
+print("max speed of modelX is", modelX.max_speed)
+print("milage of modelX is", modelX.milage)
+
+    
